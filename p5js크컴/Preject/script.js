ed5c61da10d1a4e5160b651337605d0604f04a3c
skssksdl <jch0212216@gmail.com>
2024-03-23 21:51:26 +0900

    null

--- p5js크컴/Preject/script.js
+++ p5js크컴/Preject/script.js
@@ -1,1 +1,15 @@
-console.log("찬하의 맥에 저장");
+let img;
+
+function preload() {
+    img = loadImage('./sia.png');
+
+}
+function setup() {
+    createCanvas(img.weight, img.height);
+    background(100);
+
+}
+
+function draw() {
+    image(img, 0, 0);
+}
