50293447be08f7503ee689febc7546d118ea82bb
skssksdl <jch0212216@gmail.com>
2024-03-24 01:37:22 +0900

    과제완성

---
+++ ch/script.js
@@ -0,0 +1,56 @@
+// 이 프로그램은 400 * 400 픽셀의 png 파일만 처리가 가능하다 ~
+
+let img; // 이미지를 저장할 변수
+let pixels; // 픽셀값을 저장할 변수
+let arr = new Array(50); // 50x50 크기의 배열 선언
+
+// 50x50 크기의 2차원 배열 초기화
+for (let i = 0; i < 50; i++) {
+    arr[i] = new Array(50);
+}
+
+// preload 함수: 이미지를 로드하는 함수
+function preload() {
+    img = loadImage('./sia400.png'); // 'sia400.png' 이미지 파일 로드
+
+}
+
+function setup() {
+    createCanvas(1600, 900); // 이미지와 같은 크기의 캔버스 생성
+    image(img, 0, 0); // 이미지를 캔버스에 표시
+    img.loadPixels(); // 이미지의 픽셀 정보를 불러옴
+
+    // 이미지를 8x8 픽셀 블록으로 나누어 각 블록의 평균 알파값을 구함
+    for (let height = 0; height < 400; height += 8) { // 이미지 높이를 8 픽셀 단위로 반복
+        for (let width = 0; width < 400; width += 8) { // 이미지 너비를 8 픽셀 단위로 반복
+
+            pixels = 0; // 픽셀값 초기화
+            let i, j;
+            for (j = 0; j < 8; j++) { // 블록 내 행 반복
+                for (i = 0; i < 8; i++) { // 블록 내 열 반복
+                    pixels += get(height + j, width + i)[3]; // 각 픽셀의 알파값을 합산
+                }
+            }
+            arr[width / 8][height / 8] = pixels / 64; // 각 블록의 알파값의 평균을 계산하여 배열에 저장
+
+        }
+    }
+    textSize(11)
+
+    for (let j =0; j < 50; j ++){
+        for (let i = 0; i < 50; i ++){
+
+            if (arr[j][i]>0) {
+                text("@", 400 + i * 16, j * 16, 12, 12);
+            }
+            else {
+                text(".", 400 + i * 16, j * 16, 12, 12);
+            }
+        }
+    }
+
+}
+
+function draw() {
+
+}
--- p5js크컴/prject2/dotline.js
+++ p5js크컴/prject2/dotline.js
@@ -1,7 +1,51 @@
-function setup() {
-    createCanvas(400, 400);
+let img;
+
+let pixels;
+
+let arr = new Array(50);
+for(let i = 0; i < 50; i ++) {
+    arr[i] = new Array(50);
 }
 
+function preload() {
+
+    img = loadImage('./hello.png');
+}
+
+function setup() {
+    createCanvas(img.width, img.height);
+    image(img, 0, 0);
+
+    img.loadPixels();
+
+
+
+    // 50 * 50
+    for(let height = 0; height < 400; height += 8) { // 50번 반복
+
+
+        for(let width = 0; width < 400; width += 8) { // 50번 반복
+
+            // 8 * 8 => 1
+            pixels = 0;
+            let i, j;
+            for(j = 0; j < 8; j ++) { // 64번 반복
+                for(i = 0; i < 8; i ++) {
+                    pixels += get(i, j)[3];
+                }
+            }
+            arr[width / 8][height / 8] = pixels / 64;
+
+
+
+        }
+    }
+}
+
+console.log(arr);
+
+
+
 function draw() {
-    background(220);
+
 }
