f11a62ac0eab1cde43ece53891b0adb30936a87c
skssksdl <jch0212216@gmail.com>
2024-05-02 01:30:21 +0900

    update

--- dag/mondrian/sketch.js
+++ dag/mondrian/sketch.js
@@ -1,18 +1,24 @@
 ;(() => {
+    // 캔버스 요소와 그리기 컨텍스트 가져오기
     const canvas = document.querySelector("#canvas")
     const context = canvas.getContext("2d")
 
+    // 캔버스 크기를 브라우저 창 크기로 설정
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
 
+    // 사각형 라인 너비 설정 및 사각형 배열 초기화
     context.lineWidth = 10
     const rectangles = []
-    let splitDirectionVertical = true
+    let splitDirectionVertical = true // 사각형 분할 방향 (수직: true, 수평: false)
 
+    // 마우스 클릭 이벤트 처리
     canvas.addEventListener("click", onRectangleClick)
 
+    // 사각형 그리기 함수
     function drawRectangles() {
         rectangles.forEach((rectangle) => {
+            // 사각형 그리기
             context.fillStyle = rectangle.color
             context.beginPath()
             context.rect(rectangle.x, rectangle.y, rectangle.width, rectangle.height)
@@ -22,7 +28,9 @@
         })
     }
 
+    // 마우스 클릭 이벤트 처리 함수
     function onRectangleClick(e) {
+        // 클릭된 위치의 사각형 확인
         const clickedIndex = rectangles.findIndex((rectangle) => {
             if (
                 e.x > rectangle.x &&
@@ -33,23 +41,24 @@
                 return true
             }
         })
-
+        console.log(splitDirectionVertical)
+        // 새로운 사각형 추가 또는 기존 사각형 분할
         if (clickedIndex === -1) {
             splitDirectionVertical = !splitDirectionVertical
             return
         }
 
         const clickedRectangle = rectangles[clickedIndex]
-
         rectangles.splice(clickedIndex, 1)
-
         splitRectangleAt(clickedRectangle, {
             x: e.x - clickedRectangle.x,
             y: e.y - clickedRectangle.y,
         })
     }
 
+    // 사각형 분할 함수
     function splitRectangleAt(rectangle, position) {
+        // 사각형 분할 방향에 따라 분할된 사각형 추가
         if (splitDirectionVertical) {
             rectangles.push({
                 x: rectangle.x,
@@ -82,10 +91,12 @@
             })
         }
 
+        // 사각형 분할 방향 변경 및 사각형 그리기
         splitDirectionVertical = !splitDirectionVertical
         drawRectangles()
     }
 
+    // 무작위 색상 선택 함수
     function getColor() {
         const colors = [
             "#EBEBED",
@@ -108,6 +119,7 @@
         return colors[Math.floor(Math.random() * colors.length)]
     }
 
+    // 초기 사각형 추가 및 그리기
     rectangles.push({
         x: 0,
         y: 0,
